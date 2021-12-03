7cca1d294daf0a78e0e815dade3de5d42b61e4a6
phe-sto <christophe.brun@papit.fr>
2021-12-04 00:36:46 +0100

    FIX contact form to check phones refactor JS.

--- js/mail.js
+++ js/mail.js
@@ -1,64 +1,43 @@
 /*******************************************************************************
- * when the send button is clicked
- * @return {boolean}   Boolean, true if form is okay, false on contrary.
+ * When the send button is clicked, check the form, show the modal request
+ * @return {undefined}
  **/
 document.getElementById("send-message").addEventListener("click", function (event) {
-    // if the validator does not prevent form submit
-
-    // Get the form and check for datas in it
-    const firstName = document.getElementById("form-firstname").value
-    if (firstName === "") {
-        return false;
-    }
-    const lastName = document.getElementById("form-lastname").value
-    if (lastName === "") {
-        return false;
-    }
-    let phone = document.getElementById("form-phone").value
-    if (phone === "") {
-        phone = "NON RENSEIGNE";
-    }
-    const email = document.getElementById("form-email").value
-    if (email === "") {
-        return false;
-    }
-    const message = document.getElementById("form-message").value
-    if (message === "") {
-        return false;
-    }
-    // POST values in the background the the script URL
-    $.ajax({
-        type: "post",
-        url: "https://vps1.papit.fr/contact/",
-        scriptCharset: "utf-8",
-        async: false,
-        data: {
-            "firstname": firstName,
-            "lastname": lastName,
-            "phone": phone,
-            "email": email,
-            "message": message,
-        },
-        beforeSend: function () {
-            $('#waiting-modal').modal('show');
-        },
-        success: function () {
+    // The form HTML element
+    const form = document.getElementById("contact-form");
+    // Only POST to the server if form is complete
+    if (form.reportValidity() === true) {
+        const formData = new FormData(form);
+        $.ajax({
+            type: "post",
+            url: "https://vps1.papit.fr/contact/",
+            scriptCharset: "utf-8",
+            processData: false,
+            contentType: false,
+            async: false,
+            data: formData,
+            // Show modal before sending the request
+            beforeSend: function () {
+                $('#waiting-modal').modal('show');
+            },
             // inject the alert to .messages div in our form
-            $('#contact-form').find('.messages').html('<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&#10062;</button>Votre message a été envoyé</div>');
-        },
-        error: function () {
-            $('#contact-form').find('.messages').html("<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&#10062;</button>Erreur serveur, votre message n'a pas été envoyé.</div>");
-        },
-        complete: function () {
-            // empty the form
-            $('#contact-form')[0].reset();
-            $('#waiting-modal').modal('hide');
-            setTimeout(function () {
-                $('#contact-modal').modal('hide');
-            }, 2000)
-            return true;
-        }
-    });
+            success: function () {
+                $('#contact-form').find('.messages').html('<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&#10062;</button>Votre message a été envoyé</div>');
+            },
+            error: function () {
+                $('#contact-form').find('.messages').html("<div class='alert alert-danger alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&#10062;</button>Erreur serveur, votre message n'a pas été envoyé.</div>");
+            },
+            // Once the response is received, hide the modal and reset the form
+            complete: function () {
+                // empty the form
+                $('#contact-form')[0].reset();
+                $('#waiting-modal').modal('hide');
+                setTimeout(function () {
+                    $('#contact-modal').modal('hide');
+                }, 2000)
+                return true;
+            }
+        });
+    }
     return false;
-
 }, false)
